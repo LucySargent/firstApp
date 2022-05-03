6fdb35f73d0f8fa1af1bc08b7ecb4ff6074807eb
Lucy Sargent <lucy@tanda.co>
2022-05-04 06:41:31 +1000

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   # the notation :users or :mircroposts is a symbol
   resources :microposts
   resources :users
