0388857294eb5ee4c12f2551b16d47b880541d89
Lucy Sargent <lucy@tanda.co>
2022-05-04 11:47:41 +1000

    tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  
   # the notation :users or :mircroposts is a symbol
   resources :microposts
   resources :users
-  root 'users#index'
-
+  root 'application#hello'
 end
