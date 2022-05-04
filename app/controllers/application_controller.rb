711929383cd6df5c555b2de1682fd82dae6424a5
Lucy Sargent <lucy@tanda.co>
2022-05-04 12:25:59 +1000

    dynamic titles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello, amazing world!'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  
+  get 'static_pages/contact'
   # the notation :users or :mircroposts is a symbol
   resources :microposts
   resources :users
-  root 'application#hello'
 end
